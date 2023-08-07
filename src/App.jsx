8cf078a83f9a65484b22aec6b957a9b6b3dd0d04
Toni <toni@Clarks-iMac.local>
2023-08-06 18:44:44 -0700

    Building a Navbar

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
+function App() {
+  return (
+    <di id="container">
+    <h1></h1>
+    <div id="navbar">
+      <Link to={"/"}>Home</Link>
+      <Link to={"/blue"}>Blue</Link>
+      <Link to={"/red"}>Red</Link>
+      </div>
+    <div id="main-section">
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/blue" element={<Blue/>}/>
+      <Route path="/red" element={<Red/>} />
+      </Routes>
+    </div>
+  
+</di>
+  )
+}
+
+export default App
